662bd682264af1d2f1bc93c3d318448f76ca1d3a
HidemiOishi0126 <HidemiOishi0126@users.noreply.github.com>
2019-02-19 20:28:25 +0900

    2-10

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,7 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+		int price = (coffee*2 + snack)*108/100;
+		System.out.println(moneyInHand - price);
 	}
 }
