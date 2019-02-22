ab5a59e83e97569cb1ed98454feafaf2364f1b0a
HidemiOishi0126 <HidemiOishi0126@users.noreply.github.com>
2019-02-22 11:26:45 +0900

    7-6

--- Practice/src/practice07/PTra07_01.java
+++ Practice/src/practice07/PTra07_01.java
@@ -13,10 +13,14 @@ public class PTra07_01 {
 		int[] array;
 
 		// ★ 変数arrayの配列を5個の領域で確保してください
-		int[] array = new int[5];
+		array = new int[5];
 
 		// ★ 変数arrayにそれぞれ、[1,4,7,10,13] を代入してください
-		int[] array = {1,4,7,10,13};
+		array[0] = 1;
+		array[1] = 4;
+		array[2] = 7;
+		array[3] = 10;
+		array[4] = 13;
 
 		// ★ 変数arrayに格納されている値を前から順番に全て表示してください
 		for(int score:array) {
