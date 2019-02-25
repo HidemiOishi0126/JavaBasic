c9023486a90494fba8b79c7716980da629f41280
HidemiOishi0126 <HidemiOishi0126@users.noreply.github.com>
2019-02-25 17:19:33 +0900

    10-6

---
+++ Practice/src/practice10/Car.java
@@ -0,0 +1,22 @@
+package practice10;
+
+public class Car {
+	int serialNo;
+	String color;
+	int gasoline;
+
+	int run() {
+
+		this.gasoline = this.gasoline - 1;
+		int num = new java.util.Random().nextInt(15)+1;
+		if(gasoline < 0) {
+			return -1;
+		}else {
+		return num;
+
+
+		}
+	}
+}
+
+
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -18,6 +18,51 @@ public class PTra10_06 {
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
+		Car car1 = new Car();
+		Car car2 = new Car();
+		Car car3 = new Car();
+
+		car1.color = "red";
+		car1.gasoline = 50;
+
+		car2.color = "blue";
+		car2.gasoline = 75;
+
+		car3.color = "green";
+		car3.gasoline = 90;
+
+		Car[] cars = new Car[3];
+		cars[0] = car1;
+		cars[1] = car2;
+		cars[2] = car3;
+
+
+		final int distance = 300;
+
+		for (int i = 0; i < cars.length; i++) {
+
+			System.out.println("-------------"  + cars[i].color + "----------------------");
+
+			int path = 0;
+			int n = 0;
+			while(true) {
+				int num = cars[i].run();
+				path = path + num;
+				n++;
+				if(num < 0) {
+					System.out.println("目的地に到達できませんでした");
+				break;
+				}
+				if(path >= 300) {
+
+					System.out.println("目的地まで" + n + "時間かかりました。"
+					+"残りのガソリンは" + cars[i].gasoline + "リットルです");
+					break;
+				}
+			}
+		}
+
+
 
 	}
 }
